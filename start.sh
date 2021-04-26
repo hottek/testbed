163a148ebb9953200e6dfeb0faec83561013fb53
hottek <dev@mardybum.de>
2021-04-26 23:47:51 +0200

    Initial commit

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo docker-compose up -d
+sudo docker-compose -f ./juice-shop/docker-compose.yml up -d
+sudo docker-compose -f ./firing-range/docker-compose.yml up -d
+
---
+++ stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo docker stop testbed_nginx-testbed-proxy_1
+sudo docker stop firing-range_firing-range_1
+sudo docker stop juice-shop_juice-shop_1
