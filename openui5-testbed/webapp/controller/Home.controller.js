4fbf477d92177fb94f5c7368e9198b75d4987e83
hottek <dev@mardybum.de>
2021-05-14 15:42:38 +0200

    Implement reflective xss vuln

--- openui5-testbed/webapp/controller/Home.controller.js
+++ openui5-testbed/webapp/controller/Home.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"../model/formatter"
-], function(Controller, formatter) {
+	"../model/formatter",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, formatter, JSONModel) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
@@ -9,7 +10,19 @@ sap.ui.define([
 		formatter: formatter,
 
 		onInit: function () {
+			let oData = {
+				comments : {
+					comment : ""
+				}
+			};
+			let oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
 
+		onSubmit: function () {
+			let sComment = this.getView().getModel().getProperty("/comments/comment");
+			eval(sComment);
+			alert(sComment);
 		}
 	});
 });
